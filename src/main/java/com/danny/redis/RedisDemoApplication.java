f7325de5405b5e59af2f8f616d130f6304ba8401
Danish Daood <danishdaood@gmail.com>
2019-06-19 09:34:32 +0500

    externalized redis server credentials

--- src/main/java/com/danny/redis/RedisDemoApplication.java
+++ src/main/java/com/danny/redis/RedisDemoApplication.java
@@ -1,6 +1,7 @@
 package com.danny.redis;
 
 import com.danny.redis.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +12,19 @@ import org.springframework.data.redis.core.RedisTemplate;
 @SpringBootApplication
 public class RedisDemoApplication {
 
+    @Value("${redis.host}")
+    private String REDIS_HOST;
+
+    @Value("${redis.port}")
+    private int REDIT_PORT;
+
     public static void main(String[] args) {
         SpringApplication.run(RedisDemoApplication.class, args);
     }
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("192.168.15.15", 6379);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(REDIS_HOST, REDIT_PORT);
 //		config.setPassword(RedisPassword.of("yourRedisPasswordIfAny"));
         return new JedisConnectionFactory(config);
     }
